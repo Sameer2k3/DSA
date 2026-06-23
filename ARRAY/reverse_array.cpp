6e1d76430070c8c5d38b3ea87672c48de115954d
sameer <bv014010@gmail.com>
2026-06-23 12:26:34 +0530

    added the gitignore

--- ARRAY/reverse_array.cpp
+++ ARRAY/reverse_array.cpp
@@ -9,5 +9,20 @@ int main(){
     int start=0;
     int end=size-1;
 
+    while(start<=end){
+        // step 1
+        swap(arr[start],arr[end]);
+        // step 2
+        start++;
+        // step 3
+        end--;
+
+    }
+
+    // printing the reversed array
+    for (int i = 0; i < size; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
     
 }
